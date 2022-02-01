8ca106d58e3449dc8a8473732c95efb9ac5bd0d8
icbpvvzla <icbpvvzla@gmail.com>
2022-02-01 13:37:32 -0400

    mod 1feb 2022

---
+++ iglesia_palabra_viva_aprendiendo_de_salomon.js
@@ -0,0 +1,8 @@
+const menuSerieAprendiendoDeSalomon = document.querySelector(".navegacion_serie");
+
+menuSerieAprendiendoDeSalomon.innerHTML = `
+<a href="#">Menú de serie</a>
+    <a href="aprendiendo_de_salomon_00.html">Información sobre la serie</a>
+    <a href="aprendiendo_de_salomon_0.html">Introducción</a>
+    <a href="aprendiendo_de_salomon_1.html">1. Heredero de las promesas</a>
+    <a href="aprendiendo_de_salomon_2.html">2. Elegido por gracia</a>`;
