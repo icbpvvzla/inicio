0c36190a15846a92bbe7a06b53cc78b9c24bfe60
icbpvvzla <icbpvvzla@gmail.com>
2022-02-02 13:49:52 -0400

    mod 2feb 2022

---
+++ iglesia_palabra_viva_ideal_y_relevante.js
@@ -0,0 +1,21 @@
+const menuSerieIdealYrelevante = document.querySelector(".navegacion_serie");
+
+menuSerieIdealYrelevante.innerHTML = `
+
+<a href="#">Menú de serie</a>
+<a href="ideal_y_relevante_00.html">Información sobre la serie</a>
+<a href="ideal_y_relevante_0.html">Introducción</a>
+<a href="ideal_y_relevante_1.html">1. Dos mujeres simbólicas</a>
+<a href="ideal_y_relevante_2.html">2. Trabajadora</a>
+<a href="ideal_y_relevante_3.html">3. Hacendosa en el hogar</a>
+<a href="ideal_y_relevante_4.html">4. Generosa</a>
+<a href="ideal_y_relevante_5.html">5. Principios para distribuir</a>
+<a href="ideal_y_relevante_6.html">6. Principios para ofrendar</a>
+<a href="ideal_y_relevante_7.html">7. Sabia al hablar</a>
+<a href="ideal_y_relevante_8.html">8. Sabia al hablar (2da parte)</a>
+<a href="ideal_y_relevante_9.html">9. Bondadosa al hablar</a>
+<a href="ideal_y_relevante_10.html">10. Realmente valiosa</a>
+<a href="ideal_y_relevante_11.html">11. Esposa maravillosa</a>
+<a href="ideal_y_relevante_12.html">12. El argumento de la mujer ideal</a>
+
+`;
